39004f00f904b50c0b905b4dcd831d11fd1b62d3
= <esteban_moreeste@hotmail.com>
2023-12-20 22:47:23 -0600

    Se agrega todo para realizar la convención de versionamiento de apii

---
+++ src/Infrastructure/Settings/ApiVersionConvention.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Infrastructure.Settings
+{
+    public class ApiVersionConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var namespaceController = controller.ControllerType.Namespace;
+            var apiVersion = namespaceController?.Split('.').Last().ToLower();
+            controller.ApiExplorer.GroupName = apiVersion;
+        }
+    }
+}
---
+++ src/WebApi/Controllers/V2/ExampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.V2
+{
+    [Route("api/v2/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+    }
+}
