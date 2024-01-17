a636538a143082314c4a3b02f15c8c46f3dcc143
= <esteban_moreeste@hotmail.com>
2024-01-16 22:39:46 -0600

    Se agregan stored para consultas de usuario por id, email y telefono

---
+++ db/SQL Server/Stored Procedure/sp_GetUserById.sql
@@ -0,0 +1,30 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE sp_GetUserById
+	@IdUser VARCHAR(36)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT	IdUser, 
+			FirstName, 
+			MiddleName, 
+			LastName, 
+			SecondLastName, 
+			UPPER(G.Description) AS Gender, 
+			BirthDate, 
+			Email, 
+			PhoneNumber, 
+			RegistrationDate, 
+			RegistrationUser, 
+			UpdateDate, 
+			UpdateUser
+	FROM Users U
+	LEFT JOIN Genders G ON G.IdGender = U.Gender
+	WHERE IdUser = @IdUser;
+END
+GO
