14ed0ee31342c2b52768d35f902e873ba3c9d97a
= <esteban_moreeste@hotmail.com>
2024-01-16 22:47:09 -0600

    Update INSERT Genders.sql

--- db/Parameter/INSERT Genders.sql
+++ db/Parameter/INSERT Genders.sql
@@ -1,6 +1,4 @@
 
-TRUNCATE TABLE Genders;
-
-INSERT INTO Genders (IdGender, Description) VALUES ('1', 'Male');
-INSERT INTO Genders (IdGender, Description) VALUES ('2', 'Female');
-INSERT INTO Genders (IdGender, Description) VALUES ('3', 'Other');
+INSERT INTO Genders (IdGender, Description) VALUES ('1', 'MALE');
+INSERT INTO Genders (IdGender, Description) VALUES ('2', 'FEMALE');
+INSERT INTO Genders (IdGender, Description) VALUES ('3', 'OTHER');
