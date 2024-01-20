3e0be893b9f5125c3bc2b068161de4edbb4f29e3
= <esteban_moreeste@hotmail.com>
2024-01-19 21:52:48 -0600

    Se agrega convención de constantes

--- src/Infrastructure/Settings/SwaggerConfig.cs
+++ src/Infrastructure/Settings/SwaggerConfig.cs
@@ -6,28 +6,28 @@ namespace Infrastructure.Settings
 {
     public static class SwaggerConfig
     {
-        private const string ApplicationName = Application.DependencyInjection.ApplicationName;
-        private const string Swagger = "swagger";
-        private const string Version1 = "v1";
-        private const string Version2 = "v2";
-        private const string Json = "json";
-        private const string Authorization = "Authorization";
-        private const string Bearer = "Bearer";
-        private const string Jwt = "JWT";
+        private const string APPLICATION_NAME = Application.DependencyInjection.ApplicationName;
+        private const string SWAGGER = "swagger";
+        private const string VERSION_1 = "v1";
+        private const string VERSION_2 = "v2";
+        private const string JSON = "json";
+        private const string AUTHORIZATION = "Authorization";
+        private const string BEARER = "Bearer";
+        private const string JWT = "JWT";
 
         public static IServiceCollection AddDefaultSwagger(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc(Version1, new OpenApiInfo { Title = ApplicationName, Version = Version1 });
-                c.SwaggerDoc(Version2, new OpenApiInfo { Title = ApplicationName, Version = Version2 });
+                c.SwaggerDoc(VERSION_1, new OpenApiInfo { Title = APPLICATION_NAME, Version = VERSION_1 });
+                c.SwaggerDoc(VERSION_2, new OpenApiInfo { Title = APPLICATION_NAME, Version = VERSION_2 });
 
-                c.AddSecurityDefinition(Bearer, new OpenApiSecurityScheme
+                c.AddSecurityDefinition(BEARER, new OpenApiSecurityScheme
                 {
-                    Name = Authorization,
+                    Name = AUTHORIZATION,
                     Type = SecuritySchemeType.ApiKey,
-                    Scheme = Bearer,
-                    BearerFormat = Jwt,
+                    Scheme = BEARER,
+                    BearerFormat = JWT,
                     In = ParameterLocation.Header
                 });
 
@@ -39,7 +39,7 @@ namespace Infrastructure.Settings
                             Reference = new OpenApiReference
                             {
                                 Type = ReferenceType.SecurityScheme,
-                                Id = Bearer
+                                Id = BEARER
                             }
                         },
                         new string[]{ }
@@ -55,8 +55,8 @@ namespace Infrastructure.Settings
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint($"/{Swagger}/{Version1}/{Swagger}.{Json}", $"{ApplicationName} {Version1}");
-                c.SwaggerEndpoint($"/{Swagger}/{Version2}/{Swagger}.{Json}", $"{ApplicationName} {Version2}");
+                c.SwaggerEndpoint($"/{SWAGGER}/{VERSION_1}/{SWAGGER}.{JSON}", $"{APPLICATION_NAME} {VERSION_1}");
+                c.SwaggerEndpoint($"/{SWAGGER}/{VERSION_2}/{SWAGGER}.{JSON}", $"{APPLICATION_NAME} {VERSION_2}");
             });
 
             return app;
