3726a44c9dc280b5b23f264d754b9a1a9ed5bb05
= <esteban_moreeste@hotmail.com>
2024-02-01 23:13:58 -0600

    se valida existencia del usuario y estatus

--- src/Application/Auth/Services/LoginService.cs
+++ src/Application/Auth/Services/LoginService.cs
@@ -1,12 +1,37 @@
 ﻿using Application.Auth.DTOs;
+using Domain.Repository;
 
 namespace Application.Auth.Services
 {
     public class LoginService : ILoginService
     {
+        private readonly IUserRepository _userRepository;
+
+        public LoginService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public async Task<LoginDTO> Login(string email, string password)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.GetUserByEmail(email);
+
+            if (user == null)
+            {
+                throw new Exception("No existe el usuario.");
+            }
+
+            if (user.Status == 2)
+            {
+                throw new Exception("Usuario inactivo.");
+            }
+
+            if (user.Status == 3)
+            {
+                throw new Exception("Usuario bloqueado.");
+            }
+
+            return null;
         }
     }
 }
