9db40a85eb57d5be43a05ba51726992dfeb19750
= <esteban_moreeste@hotmail.com>
2024-02-17 23:14:44 -0600

    Update ConfigureAuthentication.cs

--- src/Infrastructure/Security/ConfigureAuthentication.cs
+++ src/Infrastructure/Security/ConfigureAuthentication.cs
@@ -1,6 +1,9 @@
 ﻿using Domain.Model.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Infrastructure.Security
 {
@@ -9,6 +12,25 @@ namespace Infrastructure.Security
         public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfigurationManager configuration)
         {
             var jwt = configuration.GetSection("Jwt").Get<JwtOptions>();
+
+            if (jwt == null)
+            {
+                throw new Exception("No se han configurado los valores para el jwt.");
+            }
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwt?.Issuer,
+                    ValidAudience = jwt?.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt?.Key ?? string.Empty))
+                };
+            });
 
             return services;
         }
