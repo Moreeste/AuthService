a238b06cf09d768ca0a6dba0c0f36fd5a1361813
= <esteban_moreeste@hotmail.com>
2024-02-17 23:15:07 -0600

    Update ConfigureAuthentication.cs

--- src/Infrastructure/Security/ConfigureAuthentication.cs
+++ src/Infrastructure/Security/ConfigureAuthentication.cs
@@ -12,12 +12,7 @@ namespace Infrastructure.Security
         public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfigurationManager configuration)
         {
             var jwt = configuration.GetSection("Jwt").Get<JwtOptions>();
-
-            if (jwt == null)
-            {
-                throw new Exception("No se han configurado los valores para el jwt.");
-            }
-
+            
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
