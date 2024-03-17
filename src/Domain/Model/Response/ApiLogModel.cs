78a516f4d02cf938084c6cd02f214acd31f40270
= <esteban_moreeste@hotmail.com>
2024-03-17 00:35:01 -0600

    Agregamos estructura para guardado de logs del api

---
+++ src/Domain/Model/Response/ApiLogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Model.Response
+{
+    public class ApiLogModel
+    {
+        public string? Ip { get; set; }
+        public string? Path { get; set; }
+        public string? Method { get; set; }
+        public string? JsonRequest { get; set; }
+        public string? JsonResponse { get; set; }
+        public int StatusCode { get; set; }
+        public string? Token { get; set; }
+    }
+}
--- src/Infrastructure/Middleware/ApiLogMiddleware.cs
+++ src/Infrastructure/Middleware/ApiLogMiddleware.cs
@@ -1,6 +1,8 @@
-﻿using Domain.Repository;
+﻿using Domain.Model.Response;
+using Domain.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 using System.Text;
 
 namespace Infrastructure.Middleware
@@ -18,14 +20,24 @@ namespace Infrastructure.Middleware
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            int statusCode;
-            string? jsonResponse;
-            string? jsonRequest = await ReadRequestBody(context.Request);
-            string? path = context.Request.Path;
-            string? method = context.Request.Method;
-            string? ip = context.Connection.RemoteIpAddress?.ToString();
-            string? token = context.Request.Headers.Authorization.ToString();
-            
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var result = await ProcessHttpRequest(context, next);
+
+            stopWatch.Stop();
+            await SaveLog(result, stopWatch.Elapsed);
+        }
+
+        private async Task<ApiLogModel> ProcessHttpRequest(HttpContext context, RequestDelegate next)
+        {
+            var result = new ApiLogModel();
+            result.JsonRequest = (await ReadRequestBody(context.Request)).Replace("\n", string.Empty);
+            result.Path = context.Request.Path;
+            result.Method = context.Request.Method;
+            result.Ip = context.Connection.RemoteIpAddress?.ToString();
+            result.Token = context.Request.Headers.Authorization.ToString();
+
             var originalBodyStream = context.Response.Body;
             using (var responseBody = new MemoryStream())
             {
@@ -33,12 +45,14 @@ namespace Infrastructure.Middleware
 
                 await next(context);
 
-                jsonResponse = await ReadResponseBody(context.Response);
-                statusCode = context.Response.StatusCode;
+                result.JsonResponse = await ReadResponseBody(context.Response);
+                result.StatusCode = context.Response.StatusCode;
 
                 responseBody.Seek(0, SeekOrigin.Begin);
                 await responseBody.CopyToAsync(originalBodyStream);
             }
+
+            return result;
         }
 
         private async Task<string> ReadRequestBody(HttpRequest request)
@@ -57,6 +71,18 @@ namespace Infrastructure.Middleware
             var body = await reader.ReadToEndAsync();
             response.Body.Seek(0, SeekOrigin.Begin);
             return body;
+        }
+
+        private async Task SaveLog(ApiLogModel apiLog, TimeSpan timeSpan)
+        {
+            try
+            {
+                //await _logRepository.AddApiLog();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred saving the 'api log' - {ex.Message}.");
+            }
         }
     }
 }
