b17ba34a237d57426889529b4fa76c7d02cab00a
= <esteban_moreeste@hotmail.com>
2024-03-27 23:15:18 -0600

    Update LoginDTO.cs

--- src/Application/Auth/DTOs/LoginDTO.cs
+++ src/Application/Auth/DTOs/LoginDTO.cs
@@ -3,7 +3,6 @@
     public class LoginDTO
     {
         public string? Token { get; set; }
-        public string? RefreshToken { get; set; }
         public DateTime Expiration { get; set; }
     }
 }
