64586a7763d4aba9d5911975a0f3e083afe171b1
= <esteban_moreeste@hotmail.com>
2024-05-11 00:27:12 -0600

    Agregamos validación para los datos de la paginación

---
+++ src/Application/User/Validators/GetAllUsersValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.User.Queries;
+using FluentValidation;
+
+namespace Application.User.Validators
+{
+    public class GetAllUsersValidator : AbstractValidator<GetAllUsersQuery>
+    {
+        public GetAllUsersValidator()
+        {
+            RuleFor(x => x.Page).GreaterThan(0).LessThanOrEqualTo(1000);
+            RuleFor(x => x.PageSize).GreaterThan(0).LessThanOrEqualTo(100);
+        }
+    }
+}
