fc8ff684dd0e590c7bb2af84ae6747f972581bbe
= <esteban_moreeste@hotmail.com>
2024-06-10 22:38:38 -0600

    Update GetAllUsersValidator.cs

--- src/Application/User/Validators/GetAllUsersValidator.cs
+++ src/Application/User/Validators/GetAllUsersValidator.cs
@@ -8,6 +8,10 @@ namespace Application.User.Validators
     {
         public GetAllUsersValidator(ICommonValidations commonValidations)
         {
+            RuleFor(x => x.idProfile).MaximumLength(36);
+            RuleFor(x => x.SearchTerm).MaximumLength(20);
+            RuleFor(x => x.SortColumn).MaximumLength(10);
+            RuleFor(x => x.SortOrder).MaximumLength(4);
             RuleFor(x => x.Page).NotEmpty().MaximumLength(4).Must(commonValidations.BeValidPositiveInteger)
                 .Must(text => commonValidations.BeTextNumberWithinRange(text, 1, 1000));
             RuleFor(x => x.PageSize).NotEmpty().MaximumLength(3).Must(commonValidations.BeValidPositiveInteger)
