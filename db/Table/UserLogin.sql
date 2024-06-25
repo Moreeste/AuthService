a00c919319b977a735287b50e0a67d741866006d
= <esteban_moreeste@hotmail.com>
2024-06-24 23:29:14 -0600

    Update UserLogin.sql

--- db/Table/UserLogin.sql
+++ db/Table/UserLogin.sql
@@ -6,10 +6,12 @@ CREATE TABLE UserLogin
 	Id VARCHAR(36) DEFAULT LOWER(NEWID()),  
 	IdUser VARCHAR(36) NOT NULL,
 	LoginDate DATETIME NOT NULL,
-	Token NVARCHAR(500),
-	TokenExpiration DATETIME,
-	RefreshToken NVARCHAR(200),
-	RefreshTokenExpiration DATETIME
+	Token NVARCHAR(500) NOT NULL,
+	TokenExpiration DATETIME NOT NULL,
+	RefreshToken NVARCHAR(200) NOT NULL,
+	RefreshTokenExpiration DATETIME NOT NULL,
+	Refreshed BIT NOT NULL,
+	RefreshedBy NVARCHAR(200),
 
 	CONSTRAINT PK_UserLogin_Id PRIMARY KEY (Id)
 );
@@ -17,7 +19,10 @@ CREATE TABLE UserLogin
 CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser ON UserLogin (IdUser);
 CREATE NONCLUSTERED INDEX IX_UserLogin_Token ON UserLogin (Token);
 CREATE NONCLUSTERED INDEX IX_UserLogin_RefreshToken ON UserLogin (RefreshToken);
+CREATE NONCLUSTERED INDEX IX_UserLogin_RefreshedBy ON UserLogin (RefreshedBy);
 CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser_Token ON UserLogin (IdUser, Token);
 CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser_RefreshToken ON UserLogin (IdUser, RefreshToken);
+CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser_Refreshed ON UserLogin (IdUser, Refreshed);
+CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser_RefreshedBy ON UserLogin (IdUser, RefreshedBy)
 CREATE NONCLUSTERED INDEX IX_UserLogin_Token_RefreshToken ON UserLogin (Token, RefreshToken);
 CREATE NONCLUSTERED INDEX IX_UserLogin_IdUser_Token_RefreshToken ON UserLogin (IdUser, Token, RefreshToken);
