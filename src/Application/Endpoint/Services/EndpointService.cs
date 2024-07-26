8c7bbcc57e748a4437fc5c66fe85161b65f1d38c
= <esteban_moreeste@hotmail.com>
2024-07-25 23:56:41 -0600

    Update EndpointService.cs

--- src/Application/Endpoint/Services/EndpointService.cs
+++ src/Application/Endpoint/Services/EndpointService.cs
@@ -1,15 +1,20 @@
 ﻿using Application.Endpoint.DTOs;
 using Domain.Repository;
+using Domain.Utilities;
 
 namespace Application.Endpoint.Services
 {
     public class EndpointService : IEndpointService
     {
+        private readonly IUtilities _utilities;
         private readonly IEndpointRepository _endpointRepository;
+        private readonly IUserRepository _userRepository;
 
-        public EndpointService(IEndpointRepository endpointRepository)
+        public EndpointService(IUtilities utilities, IEndpointRepository endpointRepository, IUserRepository userRepository)
         {
+            _utilities = utilities;
             _endpointRepository = endpointRepository;
+            _userRepository = userRepository;
         }
 
         public async Task<RegisterEndpointOutDTO> RegisterEndpoint(string idUser, string path, string method)
