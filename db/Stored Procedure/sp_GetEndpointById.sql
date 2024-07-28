1df0ddf65f48e33adcc394b6e8f5e390e3143681
= <esteban_moreeste@hotmail.com>
2024-07-27 18:47:24 -0600

    Creación del stored para consultar endpoint por id

---
+++ db/Stored Procedure/sp_GetEndpointById.sql
@@ -0,0 +1,18 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE sp_GetEndpointById
+	@IdEndpoint VARCHAR(36)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT IdEndpoint, Method, Path, Active 
+	FROM Endpoints 
+	WHERE IdEndpoint = @IdEndpoint;
+
+END
+GO
