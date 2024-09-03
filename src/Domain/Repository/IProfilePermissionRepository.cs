43399d1aad11c8e5ca064979d2f9e160134df559
= <esteban_moreeste@hotmail.com>
2024-09-02 23:06:38 -0600

    Se agregan storeds al repositorio de permisos

--- src/Domain/Repository/IProfilePermissionRepository.cs
+++ src/Domain/Repository/IProfilePermissionRepository.cs
@@ -4,9 +4,9 @@ namespace Domain.Repository
 {
     public interface IProfilePermissionRepository
     {
-        Task<ProfilePermissionModel> GetProfilePermission(string idProfile, string idEndpoint);
-        Task<IEnumerable<ProfilePermissionModel>> GetProfilePermissionsByIdProfile(string idProfile);
-        Task<IEnumerable<ProfilePermissionModel>> GetProfilePermissionsByIdEndpoint(string idEndpoint);
+        Task<ProfilePermissionModel?> GetProfilePermission(string idProfile, string idEndpoint);
+        Task<IEnumerable<ProfilePermissionModel>?> GetProfilePermissionsByIdProfile(string idProfile);
+        Task<IEnumerable<ProfilePermissionModel>?> GetProfilePermissionsByIdEndpoint(string idEndpoint);
         Task<bool> RegisterProfilePermission(string idPermission, string idProfile, string idEndpoint, string registrationUser);
         Task<bool> UpdateProfilePermission(string idPermission, bool active, string updateUser);
     }
--- src/Infrastructure/Repository/ProfilePermissionRepository.cs
+++ src/Infrastructure/Repository/ProfilePermissionRepository.cs
@@ -1,6 +1,10 @@
-﻿using Domain.Model.ProfilePermission;
+﻿using Dapper;
+using Domain.Exceptions;
+using Domain.Model.ProfilePermission;
+using Domain.Model.Response;
 using Domain.Repository;
 using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repository
 {
@@ -13,29 +17,95 @@ namespace Infrastructure.Repository
             _authServiceContext = authServiceContext;
         }
 
-        public async Task<ProfilePermissionModel> GetProfilePermission(string idProfile, string idEndpoint)
+        public async Task<ProfilePermissionModel?> GetProfilePermission(string idProfile, string idEndpoint)
         {
-            throw new NotImplementedException();
+            string qry = "EXECUTE sp_GetProfilePermission @IdProfile, @IdEndpoint;";
+            var parameters = new
+            {
+                IdProfile = idProfile,
+                IdEndpoint = idEndpoint
+            };
+
+            var result = await _authServiceContext.Database.GetDbConnection().QueryFirstOrDefaultAsync<ProfilePermissionModel>(qry, parameters);
+
+            return result;
         }
 
-        public async Task<IEnumerable<ProfilePermissionModel>> GetProfilePermissionsByIdProfile(string idProfile)
+        public async Task<IEnumerable<ProfilePermissionModel>?> GetProfilePermissionsByIdProfile(string idProfile)
         {
-            throw new NotImplementedException();
+            string qry = "EXECUTE sp_GetProfilePermissionsByIdProfile @IdProfile;";
+            var parameters = new
+            {
+                IdProfile = idProfile
+            };
+
+            var result = await _authServiceContext.Database.GetDbConnection().QueryAsync<ProfilePermissionModel>(qry, parameters);
+
+            return result;
         }
 
-        public async Task<IEnumerable<ProfilePermissionModel>> GetProfilePermissionsByIdEndpoint(string idEndpoint)
+        public async Task<IEnumerable<ProfilePermissionModel>?> GetProfilePermissionsByIdEndpoint(string idEndpoint)
         {
-            throw new NotImplementedException();
+            string qry = "EXECUTE sp_GetProfilePermissionsByIdEndpoint @IdEndpoint;";
+            var parameters = new
+            {
+                IdEndpoint = idEndpoint
+            };
+
+            var result = await _authServiceContext.Database.GetDbConnection().QueryAsync<ProfilePermissionModel>(qry, parameters);
+
+            return result;
         }
 
         public async Task<bool> RegisterProfilePermission(string idPermission, string idProfile, string idEndpoint, string registrationUser)
         {
-            throw new NotImplementedException();
+            string qry = "EXECUTE sp_RegisterProfilePermission @IdPermission, @IdProfile, @IdEndpoint @RegistrationUser;";
+            var parameters = new
+            {
+                IdPermission = idPermission,
+                IdProfile = idProfile,
+                IdEndpoint = idEndpoint,
+                RegistrationUser = registrationUser
+            };
+
+            var result = await _authServiceContext.Database.GetDbConnection().QueryFirstOrDefaultAsync<DbResponse>(qry, parameters);
+
+            if (result == null)
+            {
+                throw new DataBaseException(qry, parameters);
+            }
+
+            if (!result.Success)
+            {
+                throw new DataBaseException(qry, parameters, result.ErrorMessage);
+            }
+
+            return true;
         }
 
         public async Task<bool> UpdateProfilePermission(string idPermission, bool active, string updateUser)
         {
-            throw new NotImplementedException();
+            string qry = "EXECUTE sp_UpdateProfilePermission @IdPermission, @Active, @UpdateUser;";
+            var parameters = new
+            {
+                IdPermission = idPermission,
+                Active = active,
+                UpdateUser = updateUser
+            };
+
+            var result = await _authServiceContext.Database.GetDbConnection().QueryFirstOrDefaultAsync<DbResponse>(qry, parameters);
+
+            if (result == null)
+            {
+                throw new DataBaseException(qry, parameters);
+            }
+
+            if (!result.Success)
+            {
+                throw new DataBaseException(qry, parameters, result.ErrorMessage);
+            }
+
+            return true;
         }
     }
 }
