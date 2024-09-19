faa8d977346a87648a06ee3122b94f7882dff961
= <esteban_moreeste@hotmail.com>
2024-09-18 23:32:50 -0600

    Update MainController.cs

--- src/WebApi/Controllers/MainController.cs
+++ src/WebApi/Controllers/MainController.cs
@@ -32,5 +32,23 @@ namespace WebApi.Controllers
 
             return idUser;
         }
+
+        protected string GetIdProfile()
+        {
+            string idProfile = string.Empty;
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var claimIdProfile = identity.Claims.FirstOrDefault(x => x.Type == "IdProfile");
+
+                if (claimIdProfile != null)
+                {
+                    idProfile = claimIdProfile.Value;
+                }
+            }
+
+            return idProfile;
+        }
     }
 }
