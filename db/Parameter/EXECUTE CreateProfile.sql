b1ca2551c227288c2aa6607c151b38567d54d5f6
= <esteban_moreeste@hotmail.com>
2024-10-03 23:33:25 -0600

    Update EXECUTE CreateProfile.sql

--- db/Parameter/EXECUTE CreateProfile.sql
+++ db/Parameter/EXECUTE CreateProfile.sql
@@ -6,5 +6,5 @@ EXECUTE	sp_CreateProfile
 
 EXECUTE	sp_CreateProfile 
 		@IdProfile = '11111111-1111-1111-1111-111111111111', 
-		@Description = 'BASIC', 
+		@Description = 'DEFAULT', 
 		@RegistrationUser = '00000000-0000-0000-0000-000000000000';
