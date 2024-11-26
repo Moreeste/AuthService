57438e7c2a9978742ded4a53c504cd850be7fef3
= <esteban_moreeste@hotmail.com>
2024-11-26 00:03:04 -0600

    Update sp_GetProfilePermissions.sql

--- db/Stored Procedure/sp_GetProfilePermissions.sql
+++ db/Stored Procedure/sp_GetProfilePermissions.sql
@@ -22,13 +22,36 @@ BEGIN
 	INSERT INTO @Permissions 
 	SELECT	PP.IdPermission, 
 			PP.IdProfile, 
-			P.Description AS Profile, 
+			P.Description, 
 			PP.IdEndpoint, 
-			E.Path AS Endpoint, 
+			E.Path, 
 			PP.Active 
 	FROM ProfilePermissions PP 
 	LEFT JOIN Profiles P ON P.IdProfile = PP.IdProfile 
 	LEFT JOIN Endpoints E ON E.IdEndpoint = PP.IdEndpoint;
+
+	INSERT INTO @Permissions 
+	SELECT	CASE 
+				WHEN E.IsPublic = 1 THEN 'Public' 
+				WHEN E.IsForEveryone = 1 THEN 'ForEveryone' 
+			END, 
+			P.IdProfile, 
+			P.Description, 
+			E.IdEndpoint, 
+			E.Path, 
+			E.Active 
+			FROM Profiles P, Endpoints E 
+	WHERE E.IsPublic = 1 OR E.IsForEveryone = 1;
+
+	INSERT INTO @Permissions 
+	SELECT	'AutoForAdmins', 
+			'00000000-0000-0000-0000-000000000000', 
+			'ADMIN', 
+			IdEndpoint, 
+			Path, 
+			Active 
+	FROM Endpoints 
+	WHERE IsPublic = 0 AND IsForEveryone = 0;
 
 	SELECT * FROM @Permissions ORDER BY Endpoint;
 END
